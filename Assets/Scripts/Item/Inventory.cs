f381043e42622bc3672a80c4ba3f6de17c4942e4
FeAnd <tcf1268@gmail.com>
2026-03-15 00:55:43 +0900

    [Fix] 장비 So 복제본 생성 추가

--- Assets/Scripts/Item/Inventory.cs
+++ Assets/Scripts/Item/Inventory.cs
@@ -8,36 +8,59 @@ public class Inventory : MonoBehaviour
     [SerializeField] private List<EquipmentData> _armor;
     [SerializeField] private List<EquipmentData> _shield;
     [SerializeField] private List<EquipmentData> _weapon;
+    private List<EquipmentData> _headRuntime;
+    private List<EquipmentData> _armorRuntime;
+    private List<EquipmentData> _shieldRuntime;
+    private List<EquipmentData> _weaponRuntime;
 
     private int[] _equippedNum = new int[4];
     private PlayerStat _equippedStat = new();
 
+    void Awake()
+    {
+        CopyData(_head, _headRuntime);
+        CopyData(_armor, _armorRuntime);
+        CopyData(_shield, _shieldRuntime);
+        CopyData(_weapon, _weaponRuntime);
+    }
+
+    // so 복제본 생성
+    private void CopyData(List<EquipmentData> data, List<EquipmentData> runtime)
+    {
+        runtime = new List<EquipmentData>();
+        foreach (var original in data)
+        {
+            runtime.Add(Instantiate(original));
+        }
+    }
+
+    // 장착
     public void Equip(EquipmentData equipment)
     {
         switch (equipment.equipType)
         {
             case EquipmentType.Head:
-                _head[_equippedNum[0]].Equipped = false;
+                _headRuntime[_equippedNum[0]].Equipped = false;
                 _equippedNum[0] = equipment.EquipNum;
-                _head[equipment.EquipNum].Equipped = true;
+                _headRuntime[equipment.EquipNum].Equipped = true;
                 _equippedStat.MpRegen = equipment.Effect;
                 break;
             case EquipmentType.Armor:
-                _armor[_equippedNum[1]].Equipped = false;
+                _armorRuntime[_equippedNum[1]].Equipped = false;
                 _equippedNum[1] = equipment.EquipNum;
-                _armor[equipment.EquipNum].Equipped = true;
+                _armorRuntime[equipment.EquipNum].Equipped = true;
                 _equippedStat.Hp = equipment.Effect;
                 break;
             case EquipmentType.Shield:
-                _shield[_equippedNum[2]].Equipped = false;
+                _shieldRuntime[_equippedNum[2]].Equipped = false;
                 _equippedNum[2] = equipment.EquipNum;
-                _shield[equipment.EquipNum].Equipped = true;
+                _shieldRuntime[equipment.EquipNum].Equipped = true;
                 _equippedStat.Push = equipment.Effect;
                 break;
             case EquipmentType.Weapon:
-                _weapon[_equippedNum[3]].Equipped = false;
+                _weaponRuntime[_equippedNum[3]].Equipped = false;
                 _equippedNum[3] = equipment.EquipNum;
-                _weapon[equipment.EquipNum].Equipped = true;
+                _weaponRuntime[equipment.EquipNum].Equipped = true;
                 _equippedStat.Damage = equipment.Effect;
                 break;
             default:
@@ -46,22 +69,23 @@ public class Inventory : MonoBehaviour
         GameManager.Instance.RefreshEquip.Invoke(equipment);
     }
 
+    // 장비 획득
     public EquipmentData GetItem(EquipmentType equipment, int index)
     {
         switch (equipment)
         {
             case EquipmentType.Head:
-                _head[index].Get = true;
-                return _head[index];
+                _headRuntime[index].Get = true;
+                return _headRuntime[index];
             case EquipmentType.Armor:
-                _armor[index].Get = true;
-                return _armor[index];
+                _armorRuntime[index].Get = true;
+                return _armorRuntime[index];
             case EquipmentType.Shield:
-                _shield[index].Get = true;
-                return _shield[index];
+                _shieldRuntime[index].Get = true;
+                return _shieldRuntime[index];
             case EquipmentType.Weapon:
-                _weapon[index].Get = true;
-                return _weapon[index];
+                _weaponRuntime[index].Get = true;
+                return _weaponRuntime[index];
             default:
                 return null;
         }
@@ -74,10 +98,10 @@ public class Inventory : MonoBehaviour
 
     public List<EquipmentData> GetEquipList(int index)
     {
-        if (index == 0) return _head;
-        else if (index == 1) return _armor;
-        else if (index == 2) return _shield;
-        else return _weapon;
+        if (index == 0) return _headRuntime;
+        else if (index == 1) return _armorRuntime;
+        else if (index == 2) return _shieldRuntime;
+        else return _weaponRuntime;
 
     }
 }
